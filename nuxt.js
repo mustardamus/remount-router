77fcb8929007eebf1ae93cde34db60558c88ac3e
Sebastian Senf <contact@mustardamus.com>
2017-08-29 11:06:42 +0200

    fixed typo, correctly build the options for nuxt module

--- nuxt.js
+++ nuxt.js
@@ -4,15 +4,15 @@ const remountRouter = require('./lib/remount-router')
 
 module.exports = function (moduleOptions) {
   if (typeof this.nuxt === 'undefined') {
-    throw new Error('Seems like this is not a initialized as a Nuxt.js module')
+    throw new Error('Seems like this is not initialized as a Nuxt.js module')
   }
 
   const app = express()
-  const options = Object.assign({}, moduleOptions, {
+  const options = Object.assign({}, {
     expressAppInstance: app,
     isDev: this.options.dev,
     cobtrollersPath: path.join(this.options.srcDir, 'controllers')
-  })
+  }, moduleOptions)
 
   app.use(remountRouter(options))
   this.addServerMiddleware({ path: '/', handler: app._router })
